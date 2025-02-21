fef5ff39f6e8d5603cea609f6ba85f5b3112052e
dwordpointer <kamilbatuhancan@hotmail.com>
2025-02-21 04:28:06 +0300

    .

--- server/src/data/db.js
+++ server/src/data/db.js
@@ -1,14 +1,21 @@
 const config = require("../../config");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
-    dialect: "mysql",
-    host: config.db.host,
-    define: {
-        timestamps: false
+const sequelize = new Sequelize('defaultdb', 'avnadmin', 'AVNS_G-7wNAlU8PDN1VhGlD2', {
+    host: 'mysql-1a330cf0-kamilbatuhancan-abe8.g.aivencloud.com',
+    port: 13591,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+      },
     },
-    logging:false
-});
+    define: {
+      timestamps: false,
+    },
+    logging: false,
+  });
 
 async function connect() {
     try {
